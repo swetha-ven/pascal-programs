45d45e4e7c4d040fa2f22b18eb4d7ffdfd5f2630
Swetha Venkataraman <35317839+swetha-venkataraman@users.noreply.github.com>
2019-02-16 17:22:12 -0800

    Update arraysort.pas

--- arraysort.pas
+++ arraysort.pas
@@ -10,41 +10,41 @@ VAR
   count : integer;
 
 PROCEDURE printArray(VAR arr : tArr); //procedure to print array (pass by reference)
-VAR
-  i : integer;
-BEGIN
-  FOR i := 0 TO count - 1 DO  //for loop to iterate through the array
-  write(arr[i], ' '); //print the integers in the array in one line
-END;
+  VAR
+    i : integer;
+  BEGIN
+    FOR i := 0 TO count - 1 DO  //for loop to iterate through the array
+    write(arr[i], ' '); //print the integers in the array in one line
+  END;
 
 PROCEDURE sortArray(VAR arr : tArr);  //procedure to sort array (pass by reference)
-VAR
-  i, j : integer;
-  temp : integer;
-BEGIN //sort array in ascending order (swap technique)
-  FOR i := 0 TO count - 1 DO BEGIN
-    FOR j := i + 1 TO count - 1 DO BEGIN
-      IF arr[i] > arr[j] THEN BEGIN
-        temp := arr[i];
-        arr[i] := arr[j];
-        arr[j] := temp;
+  VAR
+    i, j : integer;
+    temp : integer;
+  BEGIN //sort array in ascending order (swap technique)
+    FOR i := 0 TO count - 1 DO BEGIN
+      FOR j := i + 1 TO count - 1 DO BEGIN
+        IF arr[i] > arr[j] THEN BEGIN
+          temp := arr[i];
+          arr[i] := arr[j];
+          arr[j] := temp;
+        END;
       END;
     END;
+    writeln;  //line feed
+    writeln;
+    writeln('Sorted values: ');
   END;
-  writeln;  //line feed
-  writeln;
-  writeln('Sorted values: ');
-END;
 
-BEGIN
+BEGIN //ArraySort
   writeln;
   writeln('Original input values: ');
   count := 0;
   WHILE (NOT eoln) AND (count < MAX_LENGTH) DO  //reads the integers given in a line
-  BEGIN
-    read(vArr[count]);
-    inc(count); //count = count + 1
-  END;
+    BEGIN
+      read(vArr[count]);
+      inc(count); //count = count + 1
+    END;
   readln;
   printArray(vArr); //prints the original, unsorted input array
   sortArray(vArr);  //calls the procedure that sorts the array
